1537edec45b6237ca98d1b9cac34e66bd409b735
Gilmar <gilmar.odp@gmail.com>
2020-05-21 12:21:56 -0300

    changed

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,10 @@
-const URLPAGE = "http://http://192.168.0.199/mundo_comentado/";
-function validate(id1, id2) {
+const URLPAGE = "https://www.vemsimplificar.com.br";
+function validateQuestion(id1, id2) {
     var content1 = document.getElementById(id1).textContent;
     var content2 = document.getElementById(id2).textContent;
 
     if (content1 === "" || content2 === "") {
-        location.href = URLPAGE + "ooops/404";
+        window.alert('A resolução dessa questão ainda não está pronto, se tiver dúvidas, entre em contato conosco atráves do e-mail ou atráves do Discord.');
+        location.href = URLPAGE + "/ooops/404";
     }
 }
