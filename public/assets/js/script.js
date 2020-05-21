2f218215a9278de2e7cfcbab2dd3c4d4ea545576
Gilmar <gilmar.odp@gmail.com>
2020-05-21 12:35:57 -0300

    e

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,14 @@
 const URLPAGE = "https://www.vemsimplificar.com.br";
 function validateQuestion(id1, id2) {
-    var content1 = document.getElementById(id1).textContent;
-    var content2 = document.getElementById(id2).textContent;
+    var content1 = trim(document.getElementById(id1).textContent);
+    var content2 = trim(document.getElementById(id2).textContent);
 
     if (content1 === "" || content2 === "") {
         window.alert('A resolução dessa questão ainda não está pronto, se tiver dúvidas, entre em contato conosco atráves do e-mail ou atráves do Discord.');
         location.href = URLPAGE + "/ooops/404";
     }
+}
+
+function trim(str) {
+	return str.replace(/^\s+|\s+$/g,"");
 }
