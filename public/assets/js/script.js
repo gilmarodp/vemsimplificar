ac11fa74c4174dbed4f93d5fc7a2c4ef39081366
Gilmar <gilmar.odp@gmail.com>
2020-05-21 13:54:17 -0300

    e

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,7 +9,7 @@ function validateQuestion(id1, id2) {
     var content2 = trim(document.getElementById(id2).textContent);
 
     if (content1 === "" || content2 === "") {
-        window.alert('A resolução dessa questão ainda não está pronta, se tiver dúvidas, entre em contato conosco atráves do e-mail ou atráves do Discord.');
         location.href = URLPAGE + "/materiais/resolucoes/2020-1";
+        window.alert('A resolução dessa questão ainda não está pronta, se tiver dúvidas, entre em contato conosco atráves do e-mail ou atráves do Discord.');
     }
 }
