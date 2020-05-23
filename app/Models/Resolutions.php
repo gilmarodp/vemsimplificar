2c268249a70c0bc777d49c430cf843582d68b243
Gilmar Oliveira <gilmar.odp@gmail.com>
2020-05-22 23:42:49 -0300

    salve

--- app/Models/Resolutions.php
+++ app/Models/Resolutions.php
@@ -46,7 +46,7 @@ class Resolutions extends Model
         $stmt->execute();
         $resolution = $stmt->fetch(\PDO::FETCH_OBJ);
 
-        $resolution->date_resolution = ;
+        $resolution->date_resolution = \convertDatetimeToDate($resolution->date_resolution);
 
         return $resolution;
     }
