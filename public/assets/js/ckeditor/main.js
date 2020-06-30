fe99232533466ade5857e90aef43c6787c9d01bd
Gilmar Oliveira <gilmar.odp@gmail.com>
2020-06-30 14:19:12 -0300

    Revert "remove changes new editor HTML (ckeditor) beta (no support image)"
    This reverts commit 7377269f4565eae92b99c8cf8d48edfb667726a1.

--- public/assets/js/ckeditor/main.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.replace('content_question', {
-    language: "pt-br",
-    extraPlugins: "easyimage",
-    removePlugins: "image",
-    filebrowserUploadUrl: "base64"
-});
-
-CKEDITOR.replace('resolution_question');
-
