50065aa803ab4301cefac4d77e2fc606c8091f74
Gilmar Oliveira <gilmar.odp@gmail.com>
2020-07-02 19:33:00 -0300

    remove ckfinder

--- public/assets/js/vendor/ckeditor/main.js
+++ public/assets/js/vendor/ckeditor/main.js
@@ -3,24 +3,21 @@ if (window.location.hostname === "www.vemsimplificar.com.br") {
 	
 	CKEDITOR.replace('content_question', {
     language: "pt-br",
+<<<<<<< HEAD
     filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
     filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
     filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
     filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
     filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
     filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+=======
+>>>>>>> 79fb9d3... remove ckfinder
 	});
 } else {
 	let folder = "/vemsimplificar";
 
 	CKEDITOR.replace('content_question', {
     language: "pt-br",
-    filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
 	});
 }
 
@@ -29,24 +26,21 @@ if (window.location.hostname === "www.vemsimplificar.com.br") {
 	
 	CKEDITOR.replace('resolution_question', {
     language: "pt-br",
+<<<<<<< HEAD
     filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
     filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
     filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
     filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
     filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
     filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+=======
+>>>>>>> 79fb9d3... remove ckfinder
 	});
 } else {
 	let folder = "/vemsimplificar";
 
 	CKEDITOR.replace('resolution_question', {
     language: "pt-br",
-    filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
 	});
 }
 
