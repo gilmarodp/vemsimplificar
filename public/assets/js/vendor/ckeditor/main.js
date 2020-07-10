14df7af6d7fe23d9e01a92591adf97cd1acba64a
Gilmar Oliveira <gilmar.odp@gmail.com>
2020-07-10 17:49:48 -0300

    resolving a bug with ckeditor ;/

--- public/assets/js/vendor/ckeditor/main.js
+++ public/assets/js/vendor/ckeditor/main.js
@@ -3,16 +3,8 @@ if (window.location.hostname === "www.vemsimplificar.com.br") {
 	
 	CKEDITOR.replace('content_question', {
     language: "pt-br",
-<<<<<<< HEAD
-    filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
-=======
->>>>>>> 79fb9d3... remove ckfinder
-	});
+	
+    });
 } else {
 	let folder = "/vemsimplificar";
 
@@ -26,16 +18,8 @@ if (window.location.hostname === "www.vemsimplificar.com.br") {
 	
 	CKEDITOR.replace('resolution_question', {
     language: "pt-br",
-<<<<<<< HEAD
-    filebrowserBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html',
-    filebrowserImageBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Images',
-    filebrowserFlashBrowseUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/ckfinder.html?type=Flash',
-    filebrowserUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-    filebrowserImageUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-    filebrowserFlashUploadUrl: 'http://'+window.location.hostname+folder+'/public/assets/js/vendor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
-=======
->>>>>>> 79fb9d3... remove ckfinder
-	});
+	
+    });
 } else {
 	let folder = "/vemsimplificar";
 
