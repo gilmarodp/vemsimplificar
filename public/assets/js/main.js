2c62545e02c1d247314bd2a4d1c0e972d1ecb888
Gilmar Oliveira <gilmar.odp@gmail.com>
2020-07-14 19:31:38 -0300

    resolving bug summernote editor #2

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,47 @@
 $(document).ready(function() {
-    $('#content_question').summernote();
+    $('#content_question').summernote({
+        placeholder: 'Copie o enunciado da questão aqui :)',
+        tabsize: 2,
+        height: 200,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'hr']],
+            ['view', ['fullscreen', 'codeview']],
+            ['help', ['help']]
+        ],
+        callbacks: {
+            onChange: function(content, $editable) {
+                $('input[name="content_question"]').val(content);
+            }
+        }
+    });
 });
 
 $(document).ready(function() {
-    $('#resolution_question').summernote();
+    $('#resolution_question').summernote({
+        placeholder: 'Desenvolva a resolução da questão aqui :)',
+        tabsize: 2,
+        height: 200,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'hr']],
+            ['view', ['fullscreen', 'codeview']],
+            ['help', ['help']]
+        ]
+        callbacks: {
+            onChange: function(content, $editable) {
+                $('input[name="resolution_question"]').val(content);
+            }
+        }
+    });
 });
